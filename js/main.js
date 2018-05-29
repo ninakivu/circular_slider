4505ecd94243ae1d3e1fd03ba92c8ead3ff8c724
Nina Klemencic <nina4klemencic@gmail.com>
2018-05-29 22:19:03 +0200

    class CS

--- js/main.js
+++ js/main.js
@@ -1,27 +1,50 @@
 const svg = document.querySelector('svg')
+const body = document.querySelector('body')
+const dial = document.querySelector('dial')
 
+document.getElementById("body").addEventListener("click", function(e) {
+  console.log(e.clientX, e.clientY)
+})
+const transport = new CircularSlider({
+  color: "green",
+  range: [0, 100],
+  step: 1
+})
 
-for (x=0; x<6; x++) {
-  var circle = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "circle"
+const food = new CircularSlider({
+  color: "red",
+  range: [0, 5000],
+  step: 1
+})
+class CircularSlider {
+  constructor(options) {
+    this.numberOfCircles = document.querySelectorAll('circle').length
+    this.circle = document.createElementNS(
+      "http://www.w3.org/2000/svg", 
+      "circle" 
     );
-  const numOfCircle = document.querySelectorAll('circle').length
-  circle.setAttribute("class", "slider")  
-  circle.setAttribute("cx", "50%")
-  circle.setAttribute("cy", "50%")
-  circle.setAttribute("r", (50 * numOfCircle))
-  circle.setAttribute("id", numOfCircle)
-    console.log(circle)
-    console.log(numOfCircle)
-    svg.appendChild(circle)
-
-
-  
-  
-  
-
+    this.color = options.color
+    this.range = options.range
+    this.circle.setAttribute("cx", "50%")
+    this.circle.setAttribute("cy", "50%")
+    this.circle.setAttribute("r", (5 * this.numberOfCircles))
+    this.circle.setAttribute("style", "stroke:#ccc; fill:none")
+    this.circle.setAttribute("class", "slider")
     
+    this.mouseDown = true
+  }
+  move() {
+    console.log("move")
+  }
+}
+
+
+
+svg.appendChild(transport.circle)
+console.log(transport)
+
 
   
-}
+
+  
+  

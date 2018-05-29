b72612ff8e0adca1526b20bd713f5b85fc8f2e7a
Nina Klemencic <nina4klemencic@gmail.com>
2018-05-29 09:13:13 +0200

    svg circles

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 const svg = document.querySelector('svg')
+
+
 for (x=0; x<6; x++) {
-  const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  var circle = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+    );
   const numOfCircle = document.querySelectorAll('circle').length
+  circle.setAttribute("class", "slider")  
   circle.setAttribute("cx", "50%")
   circle.setAttribute("cy", "50%")
   circle.setAttribute("r", (50 * numOfCircle))
-  circle.setAttribute("style", "stroke:#000000; fill:none")
-    console.log(svg)
+  circle.setAttribute("id", numOfCircle)
+    console.log(circle)
     console.log(numOfCircle)
     svg.appendChild(circle)
-  }
+
+
+  
+  
+  
+
+    
+
+  
+}

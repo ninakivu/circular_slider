b4b692b3c43b3d5bd600e00a2245d7113ea7e513
Nina Klemencic <nina4klemencic@gmail.com>
2018-05-31 08:08:30 +0200

    fix dragging + colors

--- js/main.js
+++ js/main.js
@@ -1,84 +1,72 @@
-const btn = document.querySelector('button')
-const body = document.querySelector('body')
-const container = document.getElementById('container')
-console.log(container)
-
-// document.getElementById("body").addEventListener("click", function(e) {
-//   console.log(e.clientX, e.clientY)
-//   const x = e.clientX + "px"
-//   const y = e.clientY + "px"
-//   dial[0].style.transform = `translate(${x},${y})`
-// });
-
 class CircularSlider {
 	constructor(options) {
+    this.mouseDown = false
     this.circumference = 2 * Math.PI * (options.radius - 6)
     this.numberOfCircles = document.getElementsByTagName('circle').length
     this.range = options.range
     this.radius = options.radius
-    this.mouseDown = true
     this.sliderContainer = sliderContainer()
     this.svg = svg(options)
     this.progressMeter = progressMeter(options)
-    this.progressValue = progressValue(options)
-    this.dial = dial()
+    this.progressValue = progressValue(options, this.circumference)
+    this.dial = dial(options)
     this.input = input(options)
+    this.pricing = pricing(options)
   }
 
   appendNode() {
-    var xx = Math.ceil((this.radius - 5) * Math.sin(1 * Math.PI / 180)) + this.radius + "px";
-    var yy = Math.ceil((this.radius - 5) * -Math.cos(1 * Math.PI / 180)) + this.radius + "px";
-    this.dial.style.transform = "translate(" + xx + "," + yy + ")";
-    console.log(body)
-    this.svg.appendChild(this.progressMeter);
+    this.svg.appendChild(this.progressMeter)
     this.svg.appendChild(this.progressValue)
     this.sliderContainer.appendChild(this.svg)
     this.sliderContainer.appendChild(this.dial)
     this.sliderContainer.appendChild(this.input)
     this.sliderContainer.style = `z-index: ${200 - this.numberOfCircles};`
-    this.progressValue.style.strokeDasharray = this.circumference
-    this.progressMeter.addEventListener("click", this.move.bind(this))
-    this.progressValue.addEventListener("click", this.move.bind(this))
     container.appendChild(this.sliderContainer)
-    this.progressValue.style.strokeDasharray = this.circumference;
+    this.addEventListeners()
+  }
+
+  addEventListeners() {
+    this.dial.addEventListener("mousedown", () => this.mouseDown = true)
+    this.sliderContainer.addEventListener("mousemove", this.move.bind(this)) 
+    document.addEventListener("mouseup", () => this.mouseDown = false)
     this.progress(this.input.value)
   }
 
   progress(value) {
-    var progress = value / this.range[1];
-    var dashoffset = this.circumference * (1 - progress);
+    const progress = value / this.range[1];
+    const dashoffset = this.circumference * (1 - progress);
     this.progressValue.style.strokeDashoffset = dashoffset;
   };
 
-  move(e) {
-    this.update(e)
+  move (e) {
+    if(e.type !== 'click') {
+      if (!this.mouseDown || this.range[1] === 0) return;
+      this.update(e);
+    } else {
+      this.update(e);
+    }
   }
 
-  update(e) {
-    // this.sliderContainer.style = "z-index: 20"
-    let position = { x: e.pageX, y: e.pageY }
-    var dialRadius = this.dial.offsetWidth / 2;
-    var coords = {
+  update (e) {
+    const position = { x: e.pageX, y: e.pageY }
+    const dialRadius = this.dial.offsetWidth / 2;
+    const coords = {
       x: position.x - this.sliderContainer.offsetLeft,
       y: position.y - this.sliderContainer.offsetTop
     };
-    var atan = Math.atan2(coords.x - this.radius, coords.y - this.radius);
-    var deg = Math.ceil(-atan / (Math.PI / 180) + 180);
-
-    var x = Math.ceil((this.radius - 5) * Math.sin(deg * Math.PI / 180)) + this.radius + "px";
-    var y = Math.ceil((this.radius - 5) * -Math.cos(deg * Math.PI / 180)) + this.radius + "px";
-    var points = Math.ceil(deg * this.range[1] / 360);
-    this.dial.style.transform = "translate(" + x + "," + y + ")";
+    const atan = Math.atan2(coords.x - this.radius, coords.y - this.radius);
+    const deg = Math.ceil(-atan / (Math.PI / 180) + 180);
+    const x = Math.ceil((this.radius - 5) * Math.sin(deg * Math.PI / 180)) + this.radius + "px";
+    const y = Math.ceil((this.radius - 5) * - Math.cos(deg * Math.PI / 180)) + this.radius + "px";
+    const points = Math.ceil(deg * this.range[1] / 360);
+    this.dial.style.transform = `translate(${x},${y})`
+    this.pricing.textContent = "$" + points;
     this.progress(points);
-    // console.log(position)
-
   }
-
 }
 
-
 function svg (options) {
-  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("class", "progress");
   svg.setAttribute("width", options.radius * 2);
   svg.setAttribute("height", options.radius * 2);
@@ -90,8 +78,33 @@ function svg (options) {
   return svg;
 }
 
+function pricing(options) {
+  const pricing = document.createElement("span");
+  pricing.setAttribute('class', 'pricing');
+  pricing.textContent = "$" + options.range[0]
+
+  const box = document.createElement("span");
+  box.setAttribute('class', 'box');
+  box.setAttribute("style", "background-color: " + options.color);
+
+  const text = document.createElement("span");
+  text.setAttribute('class', 'text');
+  text.textContent = options.text;
+
+  const div = document.createElement("div");
+  div.setAttribute('class', 'textContainer');
+
+  div.appendChild(pricing);
+  div.appendChild(box);
+  div.appendChild(text);
+
+  document.querySelector('.price').appendChild(div)
+
+  return pricing
+}
+
 function progressMeter(options) {
-  var progressMeter = document.createElementNS(
+  const progressMeter = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "circle"
   );
@@ -103,8 +116,8 @@ function progressMeter(options) {
   return progressMeter
 }
 
-function progressValue(options) {
-  var progressValue = document.createElementNS(
+function progressValue(options, circumference) {
+  const progressValue = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "circle"
   );
@@ -114,17 +127,21 @@ function progressValue(options) {
   progressValue.setAttribute("r", options.radius - 6);
   progressValue.setAttribute("stroke-width", 12);
   progressValue.style.stroke = options.color;
+  progressValue.style.strokeDasharray = circumference
   return progressValue
 }
 
-function dial() {
-  var dial = document.createElement('div');
+function dial (options) {
+  const xx = Math.ceil((options.radius - 5) * Math.sin(1 * Math.PI / 180)) + options.radius + "px"
+  const yy = Math.ceil((options.radius - 5) * -Math.cos(1 * Math.PI / 180)) + options.radius + "px"
+  const dial = document.createElement('div');
   dial.setAttribute('class', 'dial');
+  dial.style.transform = `translate(${xx},${yy})`;
   return dial
 }
 
 function input(options) {
-  var input = document.createElement("input");
+  const input = document.createElement("input");
   input.setAttribute("type", "range");
   input.setAttribute("id", "control");
   input.setAttribute("name", "points");
@@ -132,14 +149,11 @@ function input(options) {
   input.setAttribute("max", options.range[1]);
   input.setAttribute("step", options.step);
   input.setAttribute("value", "0");
-  // input.addEventListener("input", function(event) {
-  //   this.progress(event.target.valueAsNumber);
-  // });
   return input
 }
 
 function sliderContainer () {
-  var sliderContainer = document.createElement("div");
+  const sliderContainer = document.createElement("div");
   sliderContainer.setAttribute("class", "sliderContainer");
   return sliderContainer
 }
@@ -149,38 +163,43 @@ const circleNode = new CircularSlider({
   color: "#ff3d70",
   range: [0, 100],
   radius : 40,
-  step: 1
+  step: 1,
+  text: "Transportation"
 })
-console.log(circleNode.appendNode())
+circleNode.appendNode()
 
 const circleNode2 = new CircularSlider({
-  color: "3333ff",
+  color: "#3333ff",
   range: [0, 300],
   radius : 80,
-  step: 2
+  step: 1,
+  text: "Food"  
 })
-console.log(circleNode2.appendNode())
+circleNode2.appendNode()
 
 const circleNode3 = new CircularSlider({
-  color: "4a4a4a",
+  color: "#4a4a4a",
   range: [0, 600],
   radius : 120,
-  step: 1  
+  step: 1,
+  text: "Insurance"  
 })
-console.log(circleNode3.appendNode())
+circleNode3.appendNode()
 
 const circleNode4 = new CircularSlider({
-  color: "4fdee5",
+  color: "#4fdee5",
   range: [0, 900],
   radius : 160,
-  step: 2
+  step: 1,
+  text: "Entertainment"
 })
-console.log(circleNode4.appendNode())
+circleNode4.appendNode()
 
 const circleNode5 = new CircularSlider({
-  color: "a039cf",
+  color: "#a039cf",
   range: [0, 1200],
   radius : 200,
-  step: 1  
+  step: 1,
+  text: "Health care"  
 })
-console.log(circleNode5.appendNode())
+circleNode5.appendNode()
